fdd26c30692760770851e969aa5d0cbf33f9d608
Giovanni Milan <giovanni.milan@arup.com>
2022-01-14 12:50:19 +0100

    chore: proper path

--- generate_hdf5_db.py
+++ generate_hdf5_db.py
@@ -9,28 +9,31 @@ from subroutines.HDF5 import HDF5DatasetWriterMask
 IMAGE_DIMS=(224 , 224 , 3)
 #Main Folder
 
-main_folder="C:\\Users\\filippo.giunta\\OneDrive - Arup\\Documents\\masonry_crack\\crack_detection_CNN_masonry\\img_to_evaluate\\"
+#main_folder="C:\\Users\\filippo.giunta\\OneDrive - Arup\\Documents\\masonry_crack\\crack_detection_CNN_masonry\\img_to_evaluate\\"
+
+main_folder= os.path.join(os.getcwd(),"img_to_evaluate")
 
 #Folder containing the images that need to be evaluated by the ANN
-folder_imgs="imgs\\"
+folder_imgs="imgs"
 
 #Black Mask dir
-mask_img_dir="masks\\black_mask.png"
+#mask_img_dir="masks\\black_mask.png"
+mask_img_dir = os.path.join("masks","black_mask.png")
 
 #Import black mask
-mask_img=cv2.imread(main_folder+mask_img_dir,0)
+mask_img=cv2.imread(os.path.join(main_folder,mask_img_dir),0)
 
 
 
 #Output HDF5 file output
 hdf5_output="val.hdf5"
 
-number_of_imgs=len(os.listdir(main_folder+folder_imgs))
+number_of_imgs=len(os.listdir(os.path.join(main_folder,folder_imgs)))
 
 print('Number of imgs in the folder: '+ str(number_of_imgs))
 
 
-writer = HDF5DatasetWriterMask((number_of_imgs, IMAGE_DIMS[0], IMAGE_DIMS[1], IMAGE_DIMS[2]), main_folder+hdf5_output)
+writer = HDF5DatasetWriterMask((number_of_imgs, IMAGE_DIMS[0], IMAGE_DIMS[1], IMAGE_DIMS[2]), os.path.join(main_folder,hdf5_output))
 
 
 
@@ -38,8 +41,8 @@ writer = HDF5DatasetWriterMask((number_of_imgs, IMAGE_DIMS[0], IMAGE_DIMS[1], IM
 images=[]
 labels=[]
 
-for name_img in os.listdir(main_folder+folder_imgs):
-    img = cv2.imread(os.path.join(main_folder+folder_imgs,name_img))
+for name_img in os.listdir(os.path.join(main_folder,folder_imgs)):
+    img = cv2.imread(os.path.join(main_folder,folder_imgs,name_img))
     if img is not None:
         images.append(img)
         labels.append(mask_img)
